003e25990ad9855dfd87bf895ba21bef3051da5a
cv14 <spaudyal@gmail.com>
2017-09-05 19:52:46 -0400

    cryptooooo

---
+++ CryptSolution/CryptSolution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public clas CryptSolution{
+  public boolean isCryptSolution(String[] crypt, char[][] solution) {
+
+  }
+  public static void main(String[] args) {
+
+  }
+}
