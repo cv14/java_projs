dbf07f198c9716218c94cf408f88eef84b687514
cv14 <spaudyal@gmail.com>
2017-11-09 16:05:19 -0500

    aa

--- Arcade_codeFIghts/Edge_of_theOcean/IncreasingSequence.java
+++ Arcade_codeFIghts/Edge_of_theOcean/IncreasingSequence.java
@@ -41,26 +41,42 @@ public class IncreasingSequence {
 
   public static boolean almostIncreasingSequence(int[] sequence) {
       ArrayList<Integer> seqNumbs = new ArrayList<Integer>();
+      boolean temp = true;
+      int valor_eliminado = 0;
+      int eliminar_conteo = 0;
 
       for (int i = 0; i < sequence.length; i++){
         seqNumbs.add(sequence[i]);
       }
 
       for (int i = 0; i < seqNumbs.size()-2; i++) {
-        if(seqNumbs.get(i) > seqNumbs.get(i+1)){
+
+       if(seqNumbs.get(i) >= seqNumbs.get(i+1)){
+          valor_eliminado = seqNumbs.get(i);
           seqNumbs.remove(i);
-          for (int j = 0; j < seqNumbs.size()-2; j++){
-            if(seqNumbs.get(j) > seqNumbs.get(j+1)){
-              return false;
+          eliminar_conteo++;
+          for (int j = 0; j < seqNumbs.size()-1; j++){
+            System.out.println(seqNumbs);
+            if(seqNumbs.get(j) >= seqNumbs.get(j+1)){
+              temp = false;
+              eliminar_conteo++;
+            }else{
+              temp = true;
             }
           }
+          seqNumbs.add(i, valor_eliminado);
+          System.out.println(seqNumbs);
         }
       }
-    return true;
+      if(eliminar_conteo >= 2){
+        return false;
+      }
+      System.out.println(eliminar_conteo);
+    return temp;
   }
 
   public static void main(String[] args) {
-    int[] seq = {1, 3, 2, 1};
+    int[] seq = {1, 2, 3, 4, 3, 6};
     System.out.println(almostIncreasingSequence(seq));
 
     //seq = { 1, 3, 2};
