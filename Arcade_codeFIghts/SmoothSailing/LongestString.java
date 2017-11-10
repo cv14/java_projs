61739570625952a29560c46d6386832a4fd3e0db
cv14 <spaudyal@gmail.com>
2017-11-10 14:28:13 -0500

    final

--- Arcade_codeFIghts/SmoothSailing/LongestString.java
+++ Arcade_codeFIghts/SmoothSailing/LongestString.java
@@ -24,15 +24,36 @@ Array of the longest strings, stored in the same order as in the inputArray.
 */
 
 //Using Arraylist
+import java.util.*;
 
 public class LongestString {
 
   static String[] allLongestStrings(String[] inputArray) {
+    ArrayList<String> retArr = new ArrayList<String>();
+    int longitud = 0;
 
+    retArr.add(inputArray[0]);
+    longitud = inputArray[0].length();
+
+    for (int i = 1; i < inputArray.length; i++) {
+      if(inputArray[i].length() > longitud){
+        retArr.clear();
+        retArr.add(inputArray[i]);
+        longitud = inputArray[i].length();
+      }else if(inputArray[i].length() == longitud){
+        retArr.add(inputArray[i]);
+      }
+    }
+
+    String[] reterno_matriz = new String[retArr.size()];
+    reterno_matriz = retArr.toArray(reterno_matriz);
+
+    return reterno_matriz;
   }
 
   public static void main(String[] args) {
-
-
+    String[] inputArray = {"abc","eeee","abcd","dcd"} ;
+    inputArray = allLongestStrings(inputArray);
+    System.out.println(Arrays.toString(inputArray));
   }
 }
