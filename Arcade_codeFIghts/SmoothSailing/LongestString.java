4422fdf563f65e2bf2e02507d0e9d452f41d2ac9
cv14 <spaudyal@gmail.com>
2017-11-10 13:53:57 -0500

    start

---
+++ Arcade_codeFIghts/SmoothSailing/LongestString.java
@@ -0,0 +1,38 @@
+/*
+Given an array of strings, return another array containing all of its
+longest strings.
+
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+Input/Output
+
+[time limit] 3000ms (java)
+[input] array.string inputArray
+
+A non-empty array.
+
+Guaranteed constraints:
+1 ≤ inputArray.length ≤ 10,
+1 ≤ inputArray[i].length ≤ 10.
+
+[output] array.string
+
+Array of the longest strings, stored in the same order as in the inputArray.
+*/
+
+//Using Arraylist
+
+public class LongestString {
+
+  static String[] allLongestStrings(String[] inputArray) {
+
+  }
+
+  public static void main(String[] args) {
+
+
+  }
+}
