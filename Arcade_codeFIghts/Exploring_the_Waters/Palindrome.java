b35c36a7c61fe6a897eea9febd339abc1429c7c1
cv14 <spaudyal@gmail.com>
2017-12-08 19:40:51 -0500

    start

---
+++ Arcade_codeFIghts/Exploring_the_Waters/Palindrome.java
@@ -0,0 +1,40 @@
+/*
+Given a string, find out if its characters can be rearranged to form a
+palindrome.
+
+Example
+
+For inputString = "aabb", the output should be
+palindromeRearranging(inputString) = true.
+
+We can rearrange "aabb" to make "abba", which is a palindrome.
+
+Input/Output
+
+[time limit] 3000ms (java)
+
+[input] string inputString
+
+A string consisting of lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 50.
+
+[output] boolean
+
+true if the characters of the inputString can be rearranged to form a
+palindrome, false otherwise.
+
+*/
+
+
+public class Palindrome {
+
+  static boolean palindromeRearranging(String inputString) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
