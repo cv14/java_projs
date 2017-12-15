b394358d9cf626b6bbfbc5db883849cb68d68a3c
cv14 <spaudyal@gmail.com>
2017-12-15 18:47:29 -0500

    done dona done done

--- Arcade_codeFIghts/Exploring_the_Waters/Palindrome.java
+++ Arcade_codeFIghts/Exploring_the_Waters/Palindrome.java
@@ -27,14 +27,47 @@ palindrome, false otherwise.
 
 */
 
+import java.util.* ;
 
 public class Palindrome {
 
   static boolean palindromeRearranging(String inputString) {
 
+    if(inputString.length() == 1){ return true;}
+
+    int[] alphas = new int[26];
+    boolean impars = true;
+
+    for (int i = 0; i < inputString.length(); i++) {
+      alphas[inputString.charAt(i) - 97] += 1;
+    }
+
+    //System.out.println(Arrays.toString(alphas));
+
+
+
+    if(inputString.length() % 2 == 0){
+      for (int i = 0; i < alphas.length; i++) {
+        if(alphas[i] % 2 == 1){ return false; }
+      }
+    }else {
+      for (int i = 0; i < alphas.length; i++) {
+        if(alphas[i] % 2 == 1 && impars == false){return false;}
+
+        if(alphas[i] % 2 == 1){ impars = false; }
+      }
+    }
+
+    return true;
   }
 
   public static void main(String[] args) {
+    String inStr = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbc";
+
+    System.out.println(inStr.length());
+
+    System.out.println(palindromeRearranging(inStr));
+
 
   }
 }
