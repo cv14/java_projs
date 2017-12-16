6561860023e799b7f0497d053779e889387ede01
cv14 <spaudyal@gmail.com>
2017-12-15 19:39:25 -0500

    done dona done

---
+++ Arcade_codeFIghts/Island_of_knowledge/AbsoluteDiff.java
@@ -0,0 +1,44 @@
+/*
+
+Given an array of integers, find the maximal absolute difference between any
+two of its adjacent elements.
+
+Example
+
+For inputArray = [2, 4, 1, 0], the output should be
+arrayMaximalAdjacentDifference(inputArray) = 3.
+
+Input/Output
+
+[time limit] 3000ms (java)
+
+[input] array.integer inputArray
+
+Guaranteed constraints:
+3 ≤ inputArray.length ≤ 10,
+-15 ≤ inputArray[i] ≤ 15.
+
+[output] integer
+
+The maximal absolute difference.
+*/
+
+AbsoluteDiff class ClassName {
+  static int arrayMaximalAdjacentDifference(int[] inputArray) {
+    int maximoDiferencia = 0;
+
+    for (int i = 1; i < inputArray.length; i++) {
+      if(Math.abs(inputArray[i] - inputArray[i-1]) > maximoDiferencia){
+        maximoDiferencia = Math.abs(inputArray[i] - inputArray[i-1]);
+      }
+    }
+
+    return maximoDiferencia;
+  }
+
+  public static void main(String[] args) {
+    int inputArray = {2,4,1,0};
+
+    System.out.println(arrayMaximalAdjacentDifference(inputArray));
+  }
+}
