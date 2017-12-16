4fb76a98b638efbf7ceb10ddacc1d39265f1807b
cv14 <spaudyal@gmail.com>
2017-12-15 19:08:02 -0500

    done dona done

--- Arcade_codeFIghts/Island_of_knowledge/TwoArms.java
+++ Arcade_codeFIghts/Island_of_knowledge/TwoArms.java
@@ -69,8 +69,8 @@ public class TwoArms {
   static boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft,
   int friendsRight) {
 
-    if(yourLeft == friendsLeft || yourLeft == friendsRight && yourRight == friendsLeft
-      || yourRight == friendsRight){
+    if((yourLeft == friendsLeft || yourLeft == friendsRight) && (yourRight == friendsLeft
+      || yourRight == friendsRight)){
         return true;
       }else{
         return false;
@@ -79,7 +79,7 @@ public class TwoArms {
 
 
   public static void main(String[] args) {
-    int yourLeft = 10, yourRight = 15, friendsLeft = 15, friendsRight = 10;
+    int yourLeft = 15, yourRight = 10, friendsLeft = 15, friendsRight = 9;
 
     System.out.println(areEquallyStrong(yourLeft,yourRight,friendsLeft,friendsRight));
 
