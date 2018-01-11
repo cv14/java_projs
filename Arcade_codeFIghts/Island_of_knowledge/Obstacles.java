1572e0500f173d1574ff1f3cc8e5b9714c55ed31
cv14 <spaudyal@gmail.com>
2018-01-11 16:10:01 -0500

    im back

---
+++ Arcade_codeFIghts/Island_of_knowledge/Obstacles.java
@@ -0,0 +1,42 @@
+/*
+You are given an array of integers representing coordinates of obstacles situated
+ on a straight line.
+
+Assume that you are jumping from the point with coordinate 0 to the right.
+You are allowed only to make jumps of the same length represented by some integer.
+
+Find the minimal length of the jump enough to avoid all the obstacles.
+
+Example
+
+For inputArray = [5, 3, 6, 7, 9], the output should be
+avoidObstacles(inputArray) = 4.
+
+Input/Output
+
+[time limit] 3000ms (java)
+
+[input] array.integer inputArray
+
+Non-empty array of positive integers.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 10,
+1 ≤ inputArray[i] ≤ 40.
+
+[output] integer
+
+The desired length.
+*/
+import java.util.*;
+
+public class obstacles {
+
+  static int avoidObstacles(int[] inputArray) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
